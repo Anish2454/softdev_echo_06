0027b4a16d34941c373980a9dc471186393e2c52
anish2454 <shenoy.anish.227@gmail.com>
2017-10-02 19:45:43 -0400

    Added files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def form():
+    return render_template("form.html")
+
+@app.route("/response")
+def response():
+    print "\n\n****request.args****"
+    print request.args
+    print "****request.headers****"
+    print request.headers
+    print "****request.method****"
+    print request.method
+    return render_template("response.html", name=request.args["username"], method=request.method)
+
+if (__name__ == "__main__"):
+    app.debug = True;
+    app.run()
